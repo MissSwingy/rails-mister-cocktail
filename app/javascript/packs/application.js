9006addd21cf69d660f6ae30cebad01d1035d09e
Eleonore Selly <hello@e-selly.fr>
2019-02-15 17:16:43 +0100

    Deploiement Heroku select2 commented

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,7 +11,7 @@ console.log('Hello World from Webpacker')
 
 import 'bootstrap';
 
-import 'select2/dist/css/select2.css';
-import { initSelect2 } from '../components/select2';
+// import 'select2/dist/css/select2.css';
+// import { initSelect2 } from '../components/select2';
 
-initSelect2();
+//initSelect2();
