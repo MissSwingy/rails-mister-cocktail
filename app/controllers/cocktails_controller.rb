96578ca6f72296d5ab14c224e12555ec8320b39d
Eleonore Selly <hello@e-selly.fr>
2019-02-15 12:13:56 +0100

    Solved Mister Cocktail exercise

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,14 +1,18 @@
 class CocktailsController < ApplicationController
+  before_action :set_cocktail, only: [:show, :edit, :update, :destroy]
+
   def index
     @cocktails = Cocktail.all
   end
 
   def show
-    @cocktail = Cocktail.find(params[:id])
   end
 
   def new
     @cocktail = Cocktail.new
+  end
+
+  def edit
   end
 
   def create
@@ -20,7 +24,26 @@ class CocktailsController < ApplicationController
     end
   end
 
+  def update
+    respond_to do |format|
+      if @cocktail.update(cocktail_params)
+        redirect_to cocktail_path(@cocktail)
+      else
+        render :edit
+      end
+    end
+  end
+
+  def destroy
+    @cocktail.destroy
+    redirect_to cocktails_path
+  end
+
   private
+
+  def set_cocktail
+    @cocktail = Cocktail.find(params[:id])
+  end
 
   def cocktail_params
     params.require(:cocktail).permit(:name)
